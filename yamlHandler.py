b0c5242e7ea338d08fe2d1b931902bc589ad4200
Natalia Cuevas <nataliacuevaspizarro@gmail.com>
2025-03-30 13:37:06 +0200

    Logging implemented

--- yamlHandler.py
+++ yamlHandler.py
@@ -1,4 +1,5 @@
 import yaml
+import logging
 from reportWarnings import reportWarning
 from datetime import datetime
 from ADOgroup import ADOgroup
@@ -22,12 +23,14 @@ class reportHandler:
         self.content : dict = {}
 
     def addWarnings(self, warnings: list[reportWarning]) -> None:
+        logging.debug("Adding warnings to report")
         if len(warnings) == 0:
             self.content["WARNINGS"] = "There are no warnings. CONGRATULATIONS!!! "
         else: 
             self.content["WARNINGS"] = [rWarning.toDict() for rWarning in warnings]
     
     def addBody(self, orgName: str, projectAdmins: dict[ADOgroup, list[ADOuser]])-> None:
+        logging.debug("Adding body to report")
         self.content[orgName] =  [self.formProjectBlock(group.getProjectName(), admins)
                                     for group, admins in projectAdmins.items()]
 
@@ -38,3 +41,4 @@ class reportHandler:
     def generateReport(self) -> None:
         with open(self.reportFile, 'w') as file:
             yaml.dump(self.content, file, sort_keys=False)
+        logging.info(f"Report Generated as File {self.reportFile}")
