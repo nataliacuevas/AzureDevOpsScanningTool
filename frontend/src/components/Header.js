8c98910b8a091cb0bd69329ef0efff42f098e8ec
Natalia Cuevas <nataliacuevaspizarro@gmail.com>
2025-05-06 09:54:45 -0400

    Add loading and error states to Header component
    - Introduced loading and error states in App component using useState.
    - Updated Header component to display a spinning logo during loading and an error logo when an error occurs.
    - Added CSS animations for loading and error states in Header.css.
    - Displayed loading and error messages conditionally in the Header component.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './Header.css'; // Import the CSS file
 
-function Header() {
+function Header({ isLoading, isError }) {
   return (
     <header className="header-container">
-      <img src="/pfLogo.svg" className="header-logo" alt="logo" />
+      <img 
+        src={isError ? "/pfLogoError.svg" : "/pfLogo.svg"} 
+        className={isLoading ? "header-logo-spin" : "header-logo"}
+        alt="logo" 
+      />
       <p className="header-text">
         PrimeFaktor Azure DevOps Security Analyzer <br />
         Please Provide a PAT
       </p>
+      <div className='h6vmin'>
+      <p className={isLoading ? "header-text" : "hidden-header-text"}>
+           Loading...
+        </p>        
+        <p className={isError ? "header-text" : "hidden-header-text"}>
+           Error!
+        </p>
+      </div>
+     
     </header>
   );
 }
